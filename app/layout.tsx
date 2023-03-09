73fe1b89d26f032a44644cf157d7f3d995cba007
ZIANG ZHAO <zhaoziang1227@gmail.com>
2023-03-09 12:51:03 +0200

    chore: added max width to homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className="bg-gray-50 dark:bg-zinc-900 transition-all duration-700">
         <Header />
-        <div> {children}</div>
+        <div className="max-w-6xl mx-auto"> {children}</div>
       </body>
     </html>
   );
