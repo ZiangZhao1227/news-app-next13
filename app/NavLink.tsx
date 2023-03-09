01dec5a93f49e7a55a31ceefa5ce0f9261aa57e3
ZIANG ZHAO <zhaoziang1227@gmail.com>
2023-03-09 12:14:44 +0200

    feat: added nav link section css

--- app/NavLink.tsx
+++ app/NavLink.tsx
@@ -6,9 +6,17 @@ type Props = {
 };
 
 function NavLink({ category, isActive }: Props) {
-    return <Link href={`/news/${category}`} className="nav-items">
-    {category}
-    </Link>;
+  return (
+    <Link
+      href={`/news/${category}`}
+      className={`nav-items ${
+        isActive &&
+        "underline dercoration-blue-400 underline-offset-4 font-bold text-lg"
+      }`}
+    >
+      {category}
+    </Link>
+  );
 }
 
 export default NavLink;
