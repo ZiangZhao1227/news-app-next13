d1e71f2d967b849e63e7e614eab04087a0493883
ZIANG ZHAO <zhaoziang1227@gmail.com>
2023-03-11 18:28:16 +0200

    chore: fix hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Header from "./Header";
+import Provider from "./Provider";
 
 export default function RootLayout({
   children,
@@ -12,10 +13,12 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+
       <body className="bg-gray-50 dark:bg-zinc-900 transition-all duration-700">
-        <Header />
-        <div className="max-w-6xl mx-auto"> {children}</div>
+        <Provider>
+          <Header />
+          <div className="max-w-6xl mx-auto"> {children}</div>
+        </Provider>
       </body>
     </html>
   );
