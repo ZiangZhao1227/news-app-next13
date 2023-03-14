baa974a1af1a413a4cec9fea06f48313b760606b
ZIANG ZHAO <zhaoziang1227@gmail.com>
2023-03-14 20:15:52 +0200

    chore: fix parameter typo

--- app/news/[category]/page.tsx
+++ app/news/[category]/page.tsx
@@ -21,7 +21,7 @@ async function NewsCategory({ params: { category } }: Props) {
 export default NewsCategory;
 
 export async function generateStaticParams() {
-    return categories.map((category) => ({
-      categories: category,
-    }));
-  }
+  return categories.map((category) => ({
+    category: category,
+  }));
+}
