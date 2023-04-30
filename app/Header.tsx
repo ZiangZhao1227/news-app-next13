deb13cf5f969f157f0c1f5df727b6ad87c5e9c78
ZIANG ZHAO <zhaoziang1227@gmail.com>
2023-04-30 13:42:38 +0300

    chore: remove redundant button

--- app/Header.tsx
+++ app/Header.tsx
@@ -7,8 +7,7 @@ import SearchBox from "./SearchBox";
 function Header() {
   return (
     <header>
-      <div className="grid grid-cols-3 p-10 items-center">
-        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+      <div className="p-10 items-center">
         <Link href="/" prefetch={false}>
           <h1 className="font-serif text-4xl text-center">
             My{" "}
@@ -20,9 +19,6 @@ function Header() {
         </Link>
         <div className="flex items-center justify-end gap-2">
           <DarkModeButton />
-          <button className="hidden md:inline bg-slate-900 text-white px-4 py-2 lg:px-8 lg:py-4 rounded-full dark:bg-slate-800">
-            Subscribe Now
-          </button>
         </div>
       </div>
       <NavLinks />
